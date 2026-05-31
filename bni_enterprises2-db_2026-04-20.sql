787e309233d1ee113468f7777a9195007430a7cb
yasinULLAH <115789093+yasinULLAH@users.noreply.github.com>
2026-05-31 15:19:45 +0500

    Update bni_enterprises2-db_2026-04-20.sql

--- bni_enterprises2-db_2026-04-20.sql
+++ bni_enterprises2-db_2026-04-20.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 16, 2026 at 01:10 PM
+-- Generation Time: May 31, 2026 at 11:11 AM
 -- Server version: 8.2.0
 -- PHP Version: 8.3.0
 
